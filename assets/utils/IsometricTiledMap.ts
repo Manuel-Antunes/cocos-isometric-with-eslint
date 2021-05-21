f790af3ed14261208ea1992a756301a13f015520
Manuel-Antunes <nerdbroz2@gmail.com>
2021-05-21 01:56:51 -0300

    feat: isometric tiledmap with collision systems

---
+++ assets/utils/IsometricTiledMap.ts
@@ -0,0 +1,46 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class IsometricTiledMap extends cc.TiledMap {
+  @property({ type: [cc.TiledLayer] })
+  public walkableLayers: Array<cc.TiledLayer> = [];
+
+  @property({ type: [cc.TiledLayer] })
+  public collidableLayers: Array<cc.TiledLayer> = [];
+
+  public start(): void {
+    super.start();
+    this.initMap();
+  }
+
+  private obstacles: Array<cc.Rect> = [];
+
+  private walkableTiles: Array<cc.Rect> = [];
+
+  private initMap(): void {
+    this.collidableLayers.forEach(c => {
+      this.addLayerRectsToGroup(this.obstacles, c);
+    });
+    this.walkableLayers.forEach(w => {
+      this.addLayerRectsToGroup(this.obstacles, w);
+    });
+  }
+
+  private addLayerRectsToGroup(group: Array<cc.Rect>, layer: cc.TiledLayer) {
+    for (let i = 0; i < this.getMapSize().width; i += 1) {
+      for (let j = 0; j < this.getMapSize().height; j += 1) {
+        const tile = layer.getTileGIDAt(new cc.Vec2(i, j));
+        if (tile) {
+          group.push(
+            cc.rect(
+              i * this.getTileSize().width,
+              this.getMapSize().height * this.getTileSize().height - (j + 1) * this.getMapSize().height,
+              this.getTileSize().width,
+              this.getTileSize().height
+            )
+          );
+        }
+      }
+    }
+  }
+}
